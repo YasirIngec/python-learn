6b172386595a50c4e46b12eff7c2bc33a7f7691e
Yasir <i.yasirr@hotmail.com>
2025-06-15 20:06:13 +0300

    range() fonksiyonu eklendi

--- 02_kosullar_ve_donguler/range_fonksiyonu.py
+++ 02_kosullar_ve_donguler/range_fonksiyonu.py
@@ -1,1 +1,14 @@
-print(list(range(2, 10, 2)))
+# Sıfırdan 4'e kadar sayıları yazdırır
+print("range(5):")
+for i in range(5):
+    print(i)
+
+# 2'den 10'a kadar olan çift sayıları yazdırır
+print("\nrange(2, 11, 2):")
+for i in range(2, 11, 2):
+    print(i)
+
+# 10'dan geriye doğru 1'e kadar geri sayım
+print("\nrange(10, 0, -1):")
+for i in range(10, 0, -1):
+    print(i)

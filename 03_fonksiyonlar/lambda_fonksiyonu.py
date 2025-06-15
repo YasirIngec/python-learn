a7a935436541488057b11c43cad87c2b5e707c5c
Yasir <i.yasirr@hotmail.com>
2025-06-15 20:15:16 +0300

    Lambda fonksiyonu örnekleri eklendi

--- 03_fonksiyonlar/lambda_fonksiyonu.py
+++ 03_fonksiyonlar/lambda_fonksiyonu.py
@@ -1,2 +1,20 @@
-carp = lambda x, y: x * y
-print(carp(2, 4))
+# Lambda fonksiyonları, kısa ve isimsiz fonksiyonlardır.
+
+# Normal fonksiyon:
+def karesini_al(x):
+    return x * x
+
+print(karesini_al(4))   # çıktı: 16
+
+# Aynı işlevi lambda ile:
+karesi = lambda x: x * x
+print(karesi(4))        # çıktı: 16
+
+# Birden fazla parametreyle:
+topla = lambda a, b: a + b
+print(topla(3, 7))      # çıktı: 10
+
+# Listelerde lambda kullanımı (filter, map gibi)
+sayilar = [1, 2, 3, 4, 5]
+ciftler = list(filter(lambda x: x % 2 == 0, sayilar))
+print(ciftler)          # çıktı: [2, 4]

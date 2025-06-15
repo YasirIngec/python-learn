d507e94bbaaf39623cd2049ef2f0b958efce48ac
Yasir <i.yasirr@hotmail.com>
2025-06-15 20:03:56 +0300

    Koşullu ifadeler eklendi (if-elif-else örneği)

--- 02_kosullar_ve_donguler/kosullu_ifadeler.py
+++ 02_kosullar_ve_donguler/kosullu_ifadeler.py
@@ -1,7 +1,10 @@
-x = -1
-if x > 0:
-    print('Pozitif')
-elif x == 0:
-    print('Sıfır')
+# Kullanıcının girdiği sayıya göre pozitif, negatif veya sıfır olduğunu kontrol ederiz.
+
+sayi = int(input("Bir sayı girin: "))
+
+if sayi > 0:
+    print("Pozitif sayı")
+elif sayi < 0:
+    print("Negatif sayı")
 else:
-    print('Negatif')
+    print("Sıfır")

a73b689e345d944183159e93f2c7024107ae2ae6
Yasir <i.yasirr@hotmail.com>
2025-06-15 20:13:30 +0300

    Fonksiyon tanımlama örnekleri eklendi

--- 03_fonksiyonlar/fonksiyonlar.py
+++ 03_fonksiyonlar/fonksiyonlar.py
@@ -1,4 +1,19 @@
+# Fonksiyon, belirli işlemleri topluca tanımlar ve tekrar kullanılmasını sağlar.
+
+# Parametresiz fonksiyon
+def selamla():
+    print("Merhaba, Python!")
+
+# Parametreli fonksiyon
 def topla(a, b):
     return a + b
 
-print(topla(3, 5))
+# Varsayılan parametreli fonksiyon
+def carp(a, b=2):
+    return a * b
+
+# Fonksiyonları çağıralım
+selamla()                    # çıktı: Merhaba, Python!
+print(topla(5, 3))           # çıktı: 8
+print(carp(4))               # çıktı: 8
+print(carp(4, 5))            # çıktı: 20

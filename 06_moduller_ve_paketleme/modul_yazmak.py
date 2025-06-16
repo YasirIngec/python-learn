2d6630fc8390fae050107b31290d8067de213335
Yasir <i.yasirr@hotmail.com>
2025-06-16 05:38:13 +0300

    Modul yazma ornekleri

--- 06_moduller_ve_paketleme/modul_yazmak.py
+++ 06_moduller_ve_paketleme/modul_yazmak.py
@@ -1,2 +1,19 @@
-def selamla():
-    print('Selam!')
+# Hesaplama modülü: hesap.py
+
+def topla(x, y):
+    return x + y
+
+def carp(x, y):
+    return x * y
+
+if __name__ == "__main__":
+    print("Bu modül doğrudan çalıştırıldı.")
+else:
+    print("Modül olarak import edildi.")
+
+""" # main.py
+import hesap
+
+print(hesap.topla(3, 5))  # 8
+print(hesap.carp(4, 6))   # 24
+ """

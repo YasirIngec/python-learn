ad3720327d3e84434a43804c274fa6ac29127e37
Yasir <i.yasirr@hotmail.com>
2025-06-16 05:37:46 +0300

    Modul ornekleri eklendi

--- 06_moduller_ve_paketleme/moduller.py
+++ 06_moduller_ve_paketleme/moduller.py
@@ -1,2 +1,20 @@
+# Math modülü
 import math
-print(math.sqrt(16))
+print("Karekök:", math.sqrt(16))
+print("Pi:", math.pi)
+
+# Random modülü
+import random
+print("Rastgele sayı:", random.randint(1, 10))
+
+# Datetime modülü
+from datetime import datetime
+print("Şu an:", datetime.now())
+
+# OS modülü
+import os
+print("Mevcut dizin:", os.getcwd())
+
+# SYS modülü
+import sys
+print("Python sürümü:", sys.version)
